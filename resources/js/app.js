2a2fce6ff4fb278b6626d81193e2dbdfc455821e
Hifna Wardatun Nazwa <hifnawardatunnazwa@gmail.com>
2025-05-22 16:18:58 +0700

    chore: change lang

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,10 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
+// import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
 
 window.Alpine = Alpine;
 
 Alpine.start();
 
-import '../css/powergrid-custom.css';
+// import '../css/powergrid-custom.css';
