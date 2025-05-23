e1f112c0d43227d7e704e101625650e044003adb
Hifna Wardatun Nazwa <hifnawardatunnazwa@gmail.com>
2025-05-23 16:34:19 +0700

    update lang lang lang

--- resources/lang/en/listPeserta.php
+++ resources/lang/en/listPeserta.php
@@ -2,5 +2,9 @@
 
 return[
     'title'     =>'🎓 Participant List',
-    ''
+    'search'    =>'Search by name, identification number, or study program...',
+    'peserta'   =>'Participants',
+    'halaman'   =>'per page',
+    'name'      =>'name',
+    'noinduk'   =>'',
 ];
---
+++ resources/lang/en/scoreAdmin.php
@@ -0,0 +1,5 @@
+<?php
+
+return[
+    'title'       => '📥 Import Participants Score',
+];
