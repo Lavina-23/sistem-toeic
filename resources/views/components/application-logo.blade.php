383b215bb7a0fc5d39ae4e26e7d0395bc31529b3
Lavina-23 <lavinakohai3227@gmail.com>
2025-06-03 18:58:42 +0700

    feat: update login interface and add new images

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="max-w-5xl h-[30rem]" src="{{ asset('animations/login-animation.gif') }}" alt="Animasi GIF">
+<img class="max-w-5xl h-[30rem]" src="{{ asset('images/hero-login.jpg') }}" alt="Animasi GIF">
--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,4 +1,4 @@
 <button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-yellowAccent rounded-md !font-bold text-xs text-primary hover:text-bone focus:text-bone uppercase tracking-widest hover:bg-redMain focus:bg-redMain active:bg-redMain focus:outline-none transition ease-in-out duration-150']) }}>
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-yellowAccent rounded-md font-extrabold text-md text-primary hover:text-bone focus:text-bone tracking-widest hover:bg-redMain focus:bg-redMain active:bg-redMain focus:outline-none transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
