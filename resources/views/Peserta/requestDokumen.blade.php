4c3a9806e2da7771b3782df8df0a60f1d15aa4ac
sittafatim4 <sitta.fatimaa@gmail.com>
2025-06-12 11:21:04 +0700

    fix: button download pdf

--- resources/views/Peserta/requestDokumen.blade.php
+++ resources/views/Peserta/requestDokumen.blade.php
@@ -215,7 +215,7 @@
                             </div>
 
                             <!-- Action Buttons -->
-                            <a href="{{ route('download.pdf') }}"
+                            <a 
                             class="inline-flex items-center px-6 py-3 text-sm font-medium text-white bg-yellowAccent hover:bg-yellow-500 rounded-lg focus:ring-4 focus:ring-yellow-300 transition-colors">
                              <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
